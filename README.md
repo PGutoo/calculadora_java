7c860d24b1f4454666eb428aa3587584d06c399a
Pedro Augusto <pedro.link25@gmail.com>
2022-10-06 21:48:51 -0300

    README Tratando valores diferentes do esperado

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# calculadora_java
+# Calculadora em Java
+
+Esse projeto tem o intuito de exercitar a lógica de programação utilizando Java como linguagem principal.
+
+##Funcionalidades
+A calculadora apresenta os operadores básico da aritmética, permitindo inserir quantos algarismos o usuário desejar.
+
+##Utilização
+Para utilizar o projeto bastar clonar em sua máquina e rodar o método main na classe CalculadoraApplication. Os algarismos e operadores são inseridos via console.
--- calculadora/src/main/java/com/calculadora/calculadora/CalculadoraApplication.java
+++ calculadora/src/main/java/com/calculadora/calculadora/CalculadoraApplication.java
@@ -1,7 +1,6 @@
 package com.calculadora.calculadora;
 
 import org.apache.commons.lang3.NotImplementedException;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.*;
@@ -9,13 +8,20 @@ import java.util.*;
 @SpringBootApplication
 public class CalculadoraApplication {
 
+//	@Autowired
+//	public static Aritmetica aritmetica;
+	private static final String SOMA = "+";
+	private static final String SUBTRACAO = "-";
+	private static final String MULTIPLICACAO = "*";
+	private static final String DIVISAO = "/";
+	private static final String RAIZ_QUADRADA = "%";
+
 	public static void main(String[] args) {
 		String calculadoraLigada = "ON";
 		while (calculadoraLigada.equals("ON")) {
 			Scanner usarCalculadora = new Scanner(System.in);
 			System.out.println("Digite \"ON\" para ligar ou \"OFF\" para desligar a calculadora:");
 			String respostaUsarCalculadora = usarCalculadora.nextLine();
-
 			//Qualquer valor pode entrar no Scanner, garantir que só valores numericos ou de pergunta
 			if (respostaUsarCalculadora.equals("ON") || respostaUsarCalculadora.equals("on")) {
 				while (respostaUsarCalculadora.equals("ON") || respostaUsarCalculadora.equals("on")) {
@@ -40,17 +46,28 @@ public class CalculadoraApplication {
 								}
 							}
 							//Insere Operador
-							operadorInseridoScanner = new Scanner(System.in);
-							System.out.println("Insira o operador ('+' - adição, '-' - subtração, '*' - multiplicação, '/' - divisão, '%' - raiz quadrada):");
-							operadorInserido = operadorInseridoScanner.nextLine();
-							
-							if (!operadorInserido.equals("%")) {
+							while (!operadorInserido.equals(SOMA) && !operadorInserido.equals(SUBTRACAO)
+									&& !operadorInserido.equals(MULTIPLICACAO) && !operadorInserido.equals(DIVISAO)
+									&& !operadorInserido.equals(RAIZ_QUADRADA)) {
+
+								operadorInseridoScanner = new Scanner(System.in);
+								System.out.println("Insira o operador ('+' - adição, '-' - subtração, '*' - multiplicação, '/' - divisão, '%' - raiz quadrada):");
+								operadorInserido = operadorInseridoScanner.nextLine();
+
+								if (!operadorInserido.equals(SOMA) && !operadorInserido.equals(SUBTRACAO)
+										&& !operadorInserido.equals(MULTIPLICACAO) && !operadorInserido.equals(DIVISAO)
+										&& !operadorInserido.equals(RAIZ_QUADRADA)) {
+									System.out.println("Operador inválido");
+								}
+							}
+							if (!operadorInserido.equals("%") && Objects.nonNull(operadorInserido)) {
 								//Insere Segundo valor
 								while (Objects.isNull(segundoValorInserido)) {
 									try {
 										Scanner segundoValor = new Scanner(System.in);
 										System.out.println("Valor:");
 										segundoValorInserido = segundoValor.nextDouble();
+
 									} catch (InputMismatchException e) {
 										System.out.println("Valor não numérico inserido. Por favor, insira uma valor numérico.");
 									}
@@ -75,39 +92,41 @@ public class CalculadoraApplication {
 //							System.out.println("Valor:");
 //							Double valorInserido2 = valorInseridoScanner.nextDouble();
 						//Usar hash map?
-							switch (operadorInserido) {
-								case "+":
-									System.out.println("O resultado é: " + adicao(listaDeValores));
-									Scanner desligarCalculadoraAdicao = new Scanner(System.in);
-									System.out.println("Desligar calculadora? (ON / OFF)");
-									respostaUsarCalculadora = desligarCalculadoraAdicao.nextLine();
-									break;
-								case "-":
-									System.out.println("O resultado é: " + subtracao(listaDeValores));
-									Scanner desligarCalculadoraSubtracao = new Scanner(System.in);
-									System.out.println("Desligar calculadora? (ON / OFF)");
-									respostaUsarCalculadora = desligarCalculadoraSubtracao.nextLine();
-									break;
-								case "*":
-									System.out.println("O resultado é: " + multiplicacao(listaDeValores));
-									Scanner desligarCalculadoraMultiplicacao = new Scanner(System.in);
-									System.out.println("Desligar calculadora? (ON / OFF)");
-									respostaUsarCalculadora = desligarCalculadoraMultiplicacao.nextLine();
-									break;
-								case "/":
-									System.out.println("O resultado é: " + divisao(listaDeValores));
-									Scanner desligarCalculadoraDivisao = new Scanner(System.in);
-									System.out.println("Desligar calculadora? (ON / OFF)");
-									respostaUsarCalculadora = desligarCalculadoraDivisao.nextLine();
-									break;
-								case "%":
-									System.out.println("O resultado é: " + raizQuadrada(primeiroValorInserido));
-									Scanner desligarCalculadoraRaiz = new Scanner(System.in);
-									System.out.println("Desligar calculadora? (ON / OFF)");
-									respostaUsarCalculadora = desligarCalculadoraRaiz.nextLine();
-									break;
-								default:
-									System.out.println("Operador inserido inválido:");
+						//Usar Enum
+						//TODO Testar se é possivel inserir valor vazio, caso sim, tratar
+						switch (operadorInserido) {
+							case SOMA:
+								System.out.println("O resultado é: " + adicao(listaDeValores));
+								Scanner desligarCalculadoraAdicao = new Scanner(System.in);
+								System.out.println("Desligar calculadora? (ON / OFF)");
+								respostaUsarCalculadora = desligarCalculadoraAdicao.nextLine();
+								break;
+							case SUBTRACAO:
+								System.out.println("O resultado é: " + subtracao(listaDeValores));
+								Scanner desligarCalculadoraSubtracao = new Scanner(System.in);
+								System.out.println("Desligar calculadora? (ON / OFF)");
+								respostaUsarCalculadora = desligarCalculadoraSubtracao.nextLine();
+								break;
+							case MULTIPLICACAO:
+								System.out.println("O resultado é: " + multiplicacao(listaDeValores));
+								Scanner desligarCalculadoraMultiplicacao = new Scanner(System.in);
+								System.out.println("Desligar calculadora? (ON / OFF)");
+								respostaUsarCalculadora = desligarCalculadoraMultiplicacao.nextLine();
+								break;
+							case DIVISAO:
+								System.out.println("O resultado é: " + divisao(listaDeValores));
+								Scanner desligarCalculadoraDivisao = new Scanner(System.in);
+								System.out.println("Desligar calculadora? (ON / OFF)");
+								respostaUsarCalculadora = desligarCalculadoraDivisao.nextLine();
+								break;
+							case RAIZ_QUADRADA:
+								System.out.println("O resultado é: " + raizQuadrada(primeiroValorInserido));
+								Scanner desligarCalculadoraRaiz = new Scanner(System.in);
+								System.out.println("Desligar calculadora? (ON / OFF)");
+								respostaUsarCalculadora = desligarCalculadoraRaiz.nextLine();
+								break;
+							default:
+								System.out.println("Operador inserido inválido:");
 							}
 						} catch (Exception e){
 							throw new NotImplementedException("");
